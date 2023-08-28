8b6dc39fa015f20b29374bfdbab7e094ac8ddc3b
Ollors-delights <ojoololadenofisat@gmail.com>
2023-08-28 02:21:31 -0700

    more is better

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint -  function that prints all the elements of a listint_t list
+ * @h: linked list of type listint_t to be printed
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
