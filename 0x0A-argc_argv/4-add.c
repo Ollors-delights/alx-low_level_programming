1fb68d32be0cdb69ed12dbac25c1878ede140fb7
Ollors-delights <ojoololadenofisat@gmail.com>
2023-10-07 11:22:16 -0700

    corrected

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,6 +11,7 @@
 int check_num(char *str)
 {
 	unsigned int count;
+
 	count = 0;
 	while (count < strlen(str)) /*count string*/
 
@@ -47,7 +48,7 @@ int main(int argc, char *argv[])
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
-		
+
 		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
